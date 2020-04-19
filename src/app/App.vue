df03cbc9db57c5da5ade2bdab1475c792335a33c
marvin <marvin@marvinisaac.com>
2020-04-20 06:55:22 +0800

    :construction: Setup base Vue.js app

---
+++ src/app/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>You, The Game</h1>
+        <p>{{version}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        version: process.env.VUE_APP_VERSION
+    })
+}
+</script>
