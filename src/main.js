1a42323cb1a4366a3cb96dc7682973f3c0361c35
marvin <marvin@marvinisaac.com>
2020-04-20 10:16:46 +0800

    :heavy_plus_sign: Add and setup Vuex

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './app/App.vue'
 import './app/AppStyle.scss'
+import store from './app/AppStore.js'
 
 Vue.config.productionTip = false
 
 new Vue({
+    store,
     render: h => h(App)
 }).$mount('#app')
